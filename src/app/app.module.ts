fd6037c1ed07790d537d88498385f21b4584a2b9
enmanuelcruzdejesus <enmanuelcruzdejesus@gmail.com>
2020-09-30 20:33:11 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './users/login.component';
+import { RegiserComponent } from './users/regiser.component';
+import { RegisterComponent } from './users/register.component';
+import { ShoppingCartComponent } from './invoice/shopping-cart.component';
+import { CustomerListComponent } from './customer/customer-list.component';
+import { CreateCustomerComponent } from './customer/create-customer.component';
+import { EditCustomerComponent } from './customer/edit-customer.component';
+import { ProductListComponent } from './product/product-list.component';
+import { CreateProductComponent } from './product/create-product.component';
+import { EditProductComponent } from './product/edit-product.component';
+import { MainPageComponent } from './home/main-page.component';
+import { AdminPanelComponent } from './home/admin-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegiserComponent,
+    RegisterComponent,
+    ShoppingCartComponent,
+    CustomerListComponent,
+    CreateCustomerComponent,
+    EditCustomerComponent,
+    ProductListComponent,
+    CreateProductComponent,
+    EditProductComponent,
+    MainPageComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserModule,
